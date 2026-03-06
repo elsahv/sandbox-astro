d4d1f3d3e66c7646ec9f144c1c719d50c4c5b16d
elsa <elsahvy@gmail.com>
2026-03-06 14:53:11 -0800

    add tests comps-4img banner and included pgs

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = "Laughing Coyote Dev";
-export const SITE_DESCRIPTION = "Web Development and Design";
+export const SITE_TITLE = "Sandbox-Astro 💙";
+export const SITE_DESCRIPTION = "playground for figuring out astro and react dynmaic components";
 export const HOMEPAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
